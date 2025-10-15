a56b9d01fa0aa9895c832102def241f0cb5224cd
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-10-15 13:40:49 +0000

    2.1 notes

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("hello world")
+import numpy as np
+import pandas as pd
 
+# Series object is a 1D array of indexed data
+# Like a COLUMN (one category)
+month = pd.Series(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+print(month)
+# Series have attributes values & index
+print(month.values) # looks like a list
+print(month.index) # shows the range of nums
 
-if __name__ == "__main__":
-    main()
+# You can set the index explicitly! 
+month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+better_month = pd.Series(month_list, index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+print(better_month)
+
+# Access value at index 
+print(f' My birthday is in {better_month[2]}')
+
+# Can also think of Series like a simple dictionary with key:value pairs 
+birth_months = {'Kevin': 'Mar',
+                'Jackson': 'Aug',
+                'Finny': 'Jul',
+                'Bryce': 'Nov',
+                'Natalie': 'Mar',
+                'Paige': 'Feb',
+                'Maia': 'Apr'}
+birth_series = pd.Series(birth_months)
+print(birth_series)
