d847ae5c097d099a73bf31d8c79b3347be4d6ffe
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-10-21 19:07:22 +0000

    2.1 notes (cont)

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ import pandas as pd
 # Like a COLUMN (one category)
 month = pd.Series(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 print(month)
+
+print(" ")
 # Series have attributes values & index
 print(month.values) # looks like a list
 print(month.index) # shows the range of nums
+
+print(" ")
 
 # You can set the index explicitly! 
 month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 better_month = pd.Series(month_list, index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 print(better_month)
 
+print(" ")
+
 # Access value at index 
 print(f' My birthday is in {better_month[2]}')
+
+print(" ")
 
 # Can also think of Series like a simple dictionary with key:value pairs 
 birth_months = {'Kevin': 'Mar',
@@ -27,3 +35,32 @@ birth_months = {'Kevin': 'Mar',
                 'Maia': 'Apr'}
 birth_series = pd.Series(birth_months)
 print(birth_series)
+
+print(" ")
+
+# Create a DataFrame from a dictionary
+df = pd.DataFrame(birth_series, columns=['Birth Month'])
+print(df) # DataFrame objects have column headers!
+
+print(" ")
+
+# Load tabular data from a CSV file into a DataFrame
+pokemon_df = pd.read_csv('pokemon_data.csv')
+print(pokemon_df) # [800 rows x 12 cols]
+print(pokemon_df.columns) # display col headers
+
+print(" ")
+
+# Column headers can be used to access individual columns 
+print(pokemon_df['Name'])
+
+print(" ")
+
+# Shortcut using DOT OPERATOR notation
+print(pokemon_df.HP)
+
+print(" ")
+
+# Shortcut does not work for all column names...
+# print(pokemon_df.Type 1) 
+print(pokemon_df['Type 1'])
